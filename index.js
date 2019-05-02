68063e76a6137e147d5b7632fca979111bf8d2fc
xzdev <xiaoyuzh@gmail.com>
2019-05-01 22:44:33 -0700

    update script to check the lodge changes

--- index.js
+++ index.js
@@ -1,70 +1,85 @@
-const fetch = require('node-fetch');
-const nodemailer = require('nodemailer');
-const moment = require('moment');
+const fetch = require("node-fetch");
+const nodemailer = require("nodemailer");
+const moment = require("moment");
 
-const startDate = process.argv[2] || '2019-05-24';
+const startDate = process.argv[2] || "2019-05-24";
+const emailCredential = process.argv[3] || "username:password";
 const rangeStart = moment(startDate);
-const rangeEnd = moment(rangeStart).add(7, 'days');
+const rangeEnd = moment(rangeStart).add(40, "days");
 
-
-const queryStart = rangeStart.format('ddd MMM D YYYY');
-const queryEnd = rangeEnd.format('ddd MMM D YYYY');
+const queryStart = rangeStart.format("ddd MMM D YYYY");
+const queryEnd = rangeEnd.format("ddd MMM D YYYY");
 const CHECK_INTERVAL = 30 * 3600 * 1000; // half hour
 const LINK_URL = `https://reservations.ahlsmsworld.com/Yosemite/Search/GetInventoryCountData?callback=$.wxa.on_datepicker_general_availability_loaded&CresPropCode=000000&MultiPropCode=Y&UnitTypeCode=&StartDate=${queryStart}&EndDate=${queryEnd}&_=1556410207391`;
-const RESPONE_TRIM_START = '$.wxa.on_datepicker_general_availability_loaded(';
-const RESPONE_TRIM_END = ');';
+const RESPONE_TRIM_START = "$.wxa.on_datepicker_general_availability_loaded(";
+const RESPONE_TRIM_END = ");";
 
-const sendMail = async (data) => {
-    let testAccount = await nodemailer.createTestAccount();
-    const transporter = nodemailer.createTransport({
-        host: "smtp.ethereal.email",
-        port: 587,
-        secure: false, // true for 465, false for other ports
-        auth: {
-          user: testAccount.user, // generated ethereal user
-          pass: testAccount.pass // generated ethereal password
-        }
-    });
+let avaialbeDates = [];
 
-    let info = await transporter.sendMail({
-        // from: '"Xiaoyu Zhang" <xiaoyuzh@gmail.com>', // sender address
-        // to: "xiaoyuzh@gmail.com", // list of receivers
-        from: '"Fred Foo 👻" <foo@example.com>', // sender address
-        to: "bar@example.com, baz@example.com", // list of receivers
-        subject: "Yosemite Lodge Available", // Subject line
-        text: data.map((lodge) => `${lodge.DateKey} ${lodge.AvailableCount} available`).join('\n'), // plain text body
-        html: data.map((lodge) => `<p>${lodge.DateKey} ${lodge.AvailableCount} available</p>`).join('') // html body
-    });
+const sendMail = async data => {
+  const [username, password] = emailCredential.split(":");
+  const transporter = nodemailer.createTransport({
+    pool: true,
+    host: "smtp.mail.yahoo.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: username, // generated ethereal user
+      pass: password // generated ethereal password
+    }
+  });
 
-    console.log("Message sent: %s", info.messageId);
+  let info = await transporter.sendMail({
+    from: "'someone' <someone@yahoo.com>", // sender address
+    to: "someone@gmail.com", // list of receivers
+    subject: "Yosemite Lodge Available", // Subject line
+    text: data
+      .map(lodge => `${lodge.DateKey} ${lodge.AvailableCount} available`)
+      .join("\n"), // plain text body
+    html: data
+      .map(lodge => `<p>${lodge.DateKey} ${lodge.AvailableCount} available</p>`)
+      .join("") // html body
+  });
 
-    // Preview only available when sending through an Ethereal account
-    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-}
+  console.log("Message sent: %s", info.messageId);
+};
 
 const checkLodge = async () => {
-    let foundAvailableLodge = false;
-
-    const response = await fetch(LINK_URL);
-    const responseText = await response.text();
-    const responseJson = JSON.parse(responseText.slice(RESPONE_TRIM_START.length, -RESPONE_TRIM_END.length));
-    const avaialbeDates = responseJson.filter(lodge => lodge.AvailableCount > 0);
-    avaialbeDates.forEach(lodge => {
-        if (lodge.DateKey >= rangeStart.format('YYYY-MM-DD') && lodge.DateKey <= rangeEnd.format('YYYY-MM-DD')) {
-            console.log('Found', lodge);
-            foundAvailableLodge = true;
-        }
-    })
-
-    if (foundAvailableLodge) {
-        console.log('Found available datas', avaialbeDates);
-        await sendMail(avaialbeDates);
-        if (checkInterval) {
-            clearInterval(checkInterval);
-        }
+  const response = await fetch(LINK_URL);
+  const responseText = await response.text();
+  const responseJson = JSON.parse(
+    responseText.slice(RESPONE_TRIM_START.length, -RESPONE_TRIM_END.length)
+  );
+  let newLodges = responseJson.filter(lodge => lodge.AvailableCount > 0);
+  console.log("Available lodges", newLodges);
+  newLodges = newLodges.filter((lodge, index) => {
+    if (
+      lodge.DateKey >= rangeStart.format("YYYY-MM-DD") &&
+      lodge.DateKey <= rangeEnd.format("YYYY-MM-DD")
+    ) {
+      console.log("Found", lodge);
+      return true;
     }
-}
+    return false;
+  });
+  console.log("Candidate lodges", newLodges, avaialbeDates);
 
+  // different from our last search
+  const isDifferent = newLodges.some(
+    (lodge, index) =>
+      !avaialbeDates[index] ||
+      (lodge.AvailableCount !== avaialbeDates[index].AvailableCount ||
+        lodge.DateKey !== avaialbeDates[index].DateKey)
+  );
+
+  if (isDifferent) {
+    avaialbeDates = newLodges;
+    console.log("Found available datas", avaialbeDates);
+    await sendMail(avaialbeDates);
+  } else {
+    console.log("Unchanged result");
+  }
+};
+
+setInterval(checkLodge, CHECK_INTERVAL);
 checkLodge();
-const checkInterval = setInterval(checkLodge, CHECK_INTERVAL);
